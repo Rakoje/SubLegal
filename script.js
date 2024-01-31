b35594c710ab8df63f6b8c5b338fb3001d82a8e8
Andrija Rakojevic <ra180718d@student.etf.bg.ac.rs>
2024-01-31 15:55:22 +0100

    changes in background

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // Add smooth scroll to the top when clicking a link with class "scroll-to-top"
+    $('.home-desc').on('click', '.scroll-to-top', function(event) {
+      event.preventDefault();
+      $('.home-desc').animate({ scrollTop: 0 }, 'slow');
+    });
+  });
+  
